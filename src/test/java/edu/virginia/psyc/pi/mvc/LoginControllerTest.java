ad8448e77eed891b03b6144bcea8b2abb617b175
dan <dan@giv2giv.org>
2014-03-27 12:41:28 -0400

    mend

---
+++ src/test/java/edu/virginia/psyc/pi/mvc/LoginControllerTest.java
@@ -0,0 +1,91 @@
+package edu.virginia.psyc.pi.mvc;
+
+import edu.virginia.psyc.pi.Application;
+import edu.virginia.psyc.pi.persistence.ParticipantDAO;
+import edu.virginia.psyc.pi.persistence.ParticipantRepository;
+import edu.virginia.psyc.pi.persistence.TrialRepository;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dan
+ * Date: 3/24/14
+ * Time: 10:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class LoginControllerTest {
+
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    protected ParticipantRepository participantRepository;
+
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+                .addFilters(this.springSecurityFilterChain).build();
+    }
+
+    @Test
+    public void testCreateAccountController() throws Exception {
+        this.mockMvc.perform(post("/newParticipant")
+                .param("fullName", "Dan Funk")
+                .param("email", "daniel.h.funk@gmail.com")
+                .param("unencodedPassword", "ereiamjh")
+                .param("unencodedPassword2", "ereiamjh"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/home"));
+
+        Assert.assertTrue(participantRepository.findByEmail("daniel.h.funk@gmail.com").size() > 0);
+
+    }
+
+    @Test
+    public void testLoginPostController() throws Exception {
+
+        // This will create the user.
+        testCreateAccountController();
+
+        this.mockMvc.perform(post("/login")
+                .param("username", "daniel.h.funk@gmail.com")
+                .param("password", "ereiamjh"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+
+
+
+}
