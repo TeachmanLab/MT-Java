f2b8df0f20d5cd6166c922dd72bca53113ab041e
Dan Funk <daniel.h.funk@gmail.com>
2014-04-07 14:16:25 -0400

    Adding first itteration of Interaction Training.

--- src/main/webapp/studies/int_train.js
+++ src/main/webapp/studies/int_train.js
@@ -59,8 +59,50 @@ define(['app/API'], function(API) {
 					]
 				}
 			]
-		}
+		},
+        {
+
+            input: [
+                {handle:'f',on:'keypressed', key:"f"},
+                {handle:'all_letters',on:'keypressed',key:['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']},
+            ],
+            layout: [
+                // This is a stimulus object
+                {
+                    media: {html:"<div>You are at a class that your company has sent you to. Your teacher asks each member of the group to stand up and introduce themselves. After your brief presentation, you guess the others thought you sounded <span style='white-space:nowrap'>con[ ]ident.</span></div>"},
+                    css:{fontSize:'1.2em',color:'#D7685A'}
+                }
+            ],
+            stimuli: [
+                {handle:'error', location: {top: 80}, css:{color:'red','font-size':'4em'}, media: {word:'X'}, nolog:true}
+            ],
+            interactions: [
+                // This is an interaction (it has a condition and an action)
+                {
+                    conditions: [
+                        {type:'inputEquals',value:'f'}
+                    ],
+                    actions: [
+                        {type:'endTrial'}
+                    ]
+                },
+                {
+                    conditions: [
+                        {type:'inputEquals',value:'all_letters'},
+                        {type:'inputEqualsStim', property:'letter',negate:true}
+                    ],
+                    actions: [
+                        {type:'showStim',handle:'error'},
+                        {type:'setTrialAttr', setter:{score:1}}
+                    ]
+                }
+            ]
+        }
+
 	]);
+
+
+
 
 	// #### Activate the player
 	API.play();
