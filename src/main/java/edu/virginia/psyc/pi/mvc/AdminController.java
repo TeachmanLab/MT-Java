5d6fb335f8e0776946ee230c4a614ebe14e869a1
Dan Funk <daniel.h.funk@gmail.com>
2014-04-28 00:20:12 -0400

    Adding pagination to the participant list.

--- src/main/java/edu/virginia/psyc/pi/mvc/AdminController.java
+++ src/main/java/edu/virginia/psyc/pi/mvc/AdminController.java
@@ -5,10 +5,13 @@ import edu.virginia.psyc.pi.persistence.ParticipantRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
@@ -27,6 +30,8 @@ public class AdminController {
     private ParticipantRepository participantRepository;
     private static final Logger LOG = LoggerFactory.getLogger(AdminController.class);
 
+    private static final int PER_PAGE=2;
+
     /**
      * Spring automatically configures this object.
      * You can modify the location of this database by editing the application.properties file.
@@ -37,11 +42,16 @@ public class AdminController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listUsers(ModelMap model, Principal principal ) {
+    public String listUsers(ModelMap model,
+                            final @RequestParam(value = "page", required = false, defaultValue = "1") String pageParam) {
 
-        List<ParticipantDAO> participants;
+        Page<ParticipantDAO> participants;
+        PageRequest pageRequest;
+        int page = Integer.parseInt(pageParam);
 
-        participants = participantRepository.findAll();
+        pageRequest = new PageRequest(page, PER_PAGE);
+
+        participants = participantRepository.findAll(pageRequest);
         model.addAttribute("participants", participants);
         return "admin";
 
