42b560abdf1c1d418370efe0f05c5f8349ad6f14
Dan Funk <daniel.h.funk@gmail.com>
2014-05-28 20:01:38 -0400

    Removing unneeded import.

--- src/main/java/edu/virginia/psyc/pi/mvc/LoginController.java
+++ src/main/java/edu/virginia/psyc/pi/mvc/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
