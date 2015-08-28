2a8fa8e71ead4510fccedf78cafafe32c358215e
Dan Funk <dan.funk@twintechs.com>
2015-08-28 10:05:48 -0400

    Fixing MH so the data is a bit more usable.

--- src/main/java/edu/virginia/psyc/pi/persistence/Questionnaire/MentalHealthHxTx.java
+++ src/main/java/edu/virginia/psyc/pi/persistence/Questionnaire/MentalHealthHxTx.java
@@ -28,6 +28,8 @@ public class MentalHealthHxTx implements QuestionnaireData {
 
     private String OtherDesc;
     private String OtherDescNo;
+    private String OtherHelp;
+    private String OtherHelpPast;
 
     @ElementCollection
     @CollectionTable(name = "mental_health_disorders", joinColumns = @JoinColumn(name = "id"))
@@ -39,8 +41,46 @@ public class MentalHealthHxTx implements QuestionnaireData {
     @Column(name = "Disorder_no")
     private List<String> disorders_no;
 
-    @OneToMany(cascade=CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "mentalHealthHxTx")
-    private List<MentalHealthHelpful> helpful;
+    @ElementCollection
+    @CollectionTable(name = "help", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "help")
+    private List<String> help;
+
+    @ElementCollection
+    @CollectionTable(name = "help_past", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "help_past")
+    private List<String> help_past;
+
+    private int psychiatrist;
+    private int psychologist;
+    private int school_counselor;
+    private int LMHC;
+    private int general_practitioner;
+    private int teacher;
+    private int family;
+    private int friend;
+    private int religious_leader;
+    private int coach;
+    private int book;
+    private int medicine;
+    private int online;
+    private int other;
+
+    private int psychiatrist_past;
+    private int psychologist_past;
+    private int school_counselor_past;
+    private int LMHC_past;
+    private int general_practitioner_past;
+    private int teacher_past;
+    private int family_past;
+    private int friend_past;
+    private int religious_leader_past;
+    private int coach_past;
+    private int book_past;
+    private int medicine_past;
+    private int online_past;
+    private int other_past;
+
 
 }
 
