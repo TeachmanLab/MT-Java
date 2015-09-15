4016b8041aa76919acf4932b1452ba27cd52a1a4
Dan Funk <dan.funk@twintechs.com>
2015-09-15 15:48:49 -0400

    Fixing tests.

--- src/test/java/edu/virginia/psyc/pi/DAO/OATest.java
+++ src/test/java/edu/virginia/psyc/pi/DAO/OATest.java
@@ -1,89 +1,71 @@
 package edu.virginia.psyc.pi.DAO;
 
 import edu.virginia.psyc.pi.persistence.Questionnaire.DASS21_AS;
+import edu.virginia.psyc.pi.persistence.Questionnaire.OA;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * Created with IntelliJ IDEA.
- * User: dan
- * Date: 7/31/14
- * Time: 5:37 PM
- * To change this template use File | Settings | File Templates.
- */
 public class OATest {
 
     @Test
-    public void testEligibility() {
-        DASS21_AS data;
+    public void testScore() {
+        OA data1, data2, data3, data4;
+        data1 = new OA(1,1,1,1,1);
+        data2 = new OA(4,4,4,4,4);
+        data3 = new OA(0,0,0,0,0);
 
-        data = new DASS21_AS(0,0,0,0,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,0,0,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,1,0,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,1,1,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,1,1,1,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,1,1,1,1,0);
-        assertTrue(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,1,1,1,1,1);
-        assertTrue(data.eligibleScore());
-
-        data = new DASS21_AS(0,0,2,0,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(0,2,2,0,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(2,2,2,0,0,0,0);
-        assertTrue(data.eligibleScore());
-
-        data = new DASS21_AS(0,0,3,0,0,0,0);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(0,3,3,0,0,0,0);
-        assertTrue(data.eligibleScore());
-
-        data = new DASS21_AS(3,3,3,0,0,0,0);
-        assertTrue(data.eligibleScore());
-
-        data = new DASS21_AS(-1,-1,-1,-1,-1,-1,-1);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(-1,-1,-1,-1,-1,-1,-1);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(0,-1,-1,-1,-1,-1,-1);
-        assertFalse(data.eligibleScore());
-
-        data = new DASS21_AS(1,-1,-1,-1,-1,-1,-1);
-        assertTrue(data.eligibleScore());
-
-        data = new DASS21_AS(1,1,-1,-1,-1,-1,-1);
-        assertTrue(data.eligibleScore());
+        assertEquals(data1.score(), 1, 0);
+        assertEquals(data2.score(), 4, 0);
+        assertEquals(data3.score(), 0, 0);
 
     }
 
-    @Test
-    public void testScore() {
-        DASS21_AS data1, data2, data3, data4;
-        data1 = new DASS21_AS(1,1,1,1,1,1,1);
-        data2 = new DASS21_AS(4,4,4,4,4,4,4);
-        data3 = new DASS21_AS(0,0,0,0,0,0,0);
+        @Test
+        public void testProgressWorse() {
+            List<OA> list = new ArrayList<>();
+            list.add(new OA(0,0,0,0,0));
+            list.add(new OA(1,1,1,1,1));
+            list.add(new OA(2,2,2,2,2));
+            list.add(new OA(3,3,3,3,3));
+            list.add(new OA(4,4,4,4,4));
 
-        assertEquals(data1.score(), 14, 0);
-        assertEquals(data2.score(), 56, 0);
-        assertEquals(data3.score(), 0, 0);
+            assertEquals(OA.progress(list), "worse");
+        }
+
+    @Test
+    public void testProgressSame() {
+        List<OA> list = new ArrayList<>();
+        list.add(new OA(2,2,2,2,2));
+        list.add(new OA(3,3,3,3,3));
+        list.add(new OA(1,1,1,1,1));
+        list.add(new OA(2,2,2,2,2));
+
+        assertEquals(OA.progress(list), "same");
+    }
+
+    @Test
+    public void testProgressBetter() {
+        List<OA> list = new ArrayList<>();
+        list.add(new OA(4,4,4,4,4));
+        list.add(new OA(3,3,3,3,3));
+        list.add(new OA(2,2,2,2,2));
+        list.add(new OA(1,1,1,1,1));
+        list.add(new OA(0,0,0,0,0));
+
+        assertEquals(OA.progress(list), "little better");
+    }
+
+    @Test
+    public void testProgressLotBetter() {
+        List<OA> list = new ArrayList<>();
+        list.add(new OA(4,4,4,4,4));
+        list.add(new OA(0,0,0,0,0));
+
+        assertEquals(OA.progress(list), "lot better");
     }
 
 
