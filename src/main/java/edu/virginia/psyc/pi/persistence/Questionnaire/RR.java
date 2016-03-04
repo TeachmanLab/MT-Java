f6123b59d4349a9c419a4e73ad112ff9b605ae96
Diheng Zhang <dihengz@gmail.com>
2016-03-04 16:39:57 -0500

    Bugs fixed

--- src/main/java/edu/virginia/psyc/pi/persistence/Questionnaire/RR.java
+++ src/main/java/edu/virginia/psyc/pi/persistence/Questionnaire/RR.java
@@ -21,40 +21,51 @@ import java.util.Date;
 @Data
 public class RR extends QuestionnaireData {
 
-    private int yellow1;
-    private int yellow2;
-    private int yellow3;
-    private int yellow4;
 
-    private int coffee1;
-    private int coffee2;
-    private int coffee3;
-    private int coffee4;
-
-    private int elevator1;
-    private int elevator2;
-    private int elevator3;
-    private int elevator4;
-
-    private int flight1;
-    private int flight2;
-    private int flight3;
-    private int flight4;
-
-    private int job1;
-    private int job2;
-    private int job3;
-    private int job4;
+    private int wedding1;
+    private int wedding2;
+    private int wedding3;
+    private int wedding4;
 
     private int noise1;
     private int noise2;
     private int noise3;
     private int noise4;
 
+    private int meeting_friend1;
+    private int meeting_friend2;
+    private int meeting_friend3;
+    private int meeting_friend4;
 
-    private int restaurant1;
-    private int restaurant2;
-    private int restaurant3;
-    private int restaurant4;
+    private int elevator1;
+    private int elevator2;
+    private int elevator3;
+    private int elevator4;
+
+
+    private int job1;
+    private int job2;
+    private int job3;
+    private int job4;
+
+    private int lunch1;
+    private int lunch2;
+    private int lunch3;
+    private int lunch4;
+
+    private int scrape1;
+    private int scrape2;
+    private int scrape3;
+    private int scrape4;
+
+    private int blood_test1;
+    private int blood_test2;
+    private int blood_test3;
+    private int blood_test4;
+
+    private int shopping1;
+    private int shopping2;
+    private int shopping3;
+    private int shopping4;
 
 }
