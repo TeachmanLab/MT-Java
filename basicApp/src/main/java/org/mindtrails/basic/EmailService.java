67473500d9c4f0af18206dfae4bd6d5240decdc7
Dan Funk <dan.funk@twintechs.com>
2016-05-17 13:44:12 -0400

    Adding a basic application - so I can test what is needed by a bare bones app, while keeping the current study functional.

---
+++ basicApp/src/main/java/org/mindtrails/basic/EmailService.java
@@ -0,0 +1,15 @@
+package org.mindtrails.basic;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by dan on 5/13/16.
+ */
+@Service
+public class EmailService implements edu.virginia.psyc.mindtrails.service.EmailService {
+
+    @Override
+    public void sendExportAlertEmail(String message) {
+        // Meh.
+    }
+}
