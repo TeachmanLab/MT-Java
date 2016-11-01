5d5d39c53fed04c18d0c7b11ec1df81c39c125d3
Diheng Zhang <dihengz@gmail.com>
2016-11-01 16:55:57 -0400

    Change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,10 @@ extensions = [
     'sphinx.ext.coverage',
 ]
 
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -113,7 +117,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
