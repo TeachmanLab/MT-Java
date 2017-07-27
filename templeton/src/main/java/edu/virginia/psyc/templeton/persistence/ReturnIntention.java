479a385fca9f6399ec3f325e89edc16a4d1d76f1
Diheng Zhang <dihengz@gmail.com>
2017-07-27 16:06:54 -0400

    Added new questionnaire to Templeton

---
+++ templeton/src/main/java/edu/virginia/psyc/templeton/persistence/ReturnIntention.java
@@ -0,0 +1,21 @@
+package edu.virginia.psyc.templeton.persistence;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.mindtrails.domain.questionnaire.SecureQuestionnaireData;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Diheng on 7/27/17.
+ */
+
+@Entity
+@Table(name="ReturnIntention")
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ReturnIntention extends SecureQuestionnaireData {
+    private int ReturnIntention;
+    private String notReturnReason;
+}
