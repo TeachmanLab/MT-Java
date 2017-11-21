adf26cc3d8b4444691a5787198772cbdc317b9a0
Dan Funk <daniel.h.funk@gmail.com>
2017-11-21 12:26:45 -0500

    Removing Mobile.

--- mobile/src/main/java/edu/virginia/psyc/mobile/persistence/DASS21ASRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.virginia.psyc.mobile.persistence;
-
-import org.mindtrails.persistence.QuestionnaireRepository;
-
-
-public interface DASS21ASRepository extends QuestionnaireRepository<DASS21AS> {}
-
