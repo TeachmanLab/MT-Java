3cdd13b84e6696d0c78fab28c97da3d6ad580998
diheng <dihengz@gmail.com>
2018-03-06 18:08:38 -0500

    most recent

--- core/src/main/java/org/mindtrails/domain/DataOnlyAspect.java
+++ core/src/main/java/org/mindtrails/domain/DataOnlyAspect.java
@@ -10,23 +10,23 @@ import org.springframework.beans.factory.annotation.Value;
 @Aspect
 public class DataOnlyAspect {
 
-@Value("${mode}")
-private String serverMode;
+    @Value("${mode}")
+    private String serverMode;
 
-private static final Logger LOGGER = LoggerFactory.getLogger(DataOnlyAspect.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataOnlyAspect.class);
 
 
-@Around("@annotation(DataOnly)")
-public Object checkServerType(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("@annotation(DataOnly)")
+    public Object checkServerType(ProceedingJoinPoint joinPoint) throws Throwable {
 
         LOGGER.info("Server mode dependent method: Checking server mode now:");
 
         if (serverMode.toLowerCase().equals("data")) {
-        Object proceed = joinPoint.proceed();
-        return proceed;
+            Object proceed = joinPoint.proceed();
+            return proceed;
         } else {
-        return ("/dataOnly");
+            return ("/dataOnly");
         }
 
-        }
-        }
+    }
+}
