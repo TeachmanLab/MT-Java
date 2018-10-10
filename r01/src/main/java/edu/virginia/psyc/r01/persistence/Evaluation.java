b52dab4338c7b9ecab8a54ab68d251cf1081b164
Dan Funk <daniel.h.funk@gmail.com>
2018-10-10 14:21:39 -0400

    Updating evaluation to clean up the class attributes and inforce some server side checks on values.

--- r01/src/main/java/edu/virginia/psyc/r01/persistence/Evaluation.java
+++ r01/src/main/java/edu/virginia/psyc/r01/persistence/Evaluation.java
@@ -7,58 +7,62 @@ import org.mindtrails.domain.questionnaire.LinkedQuestionnaireData;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="Evaluation")
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Evaluation extends LinkedQuestionnaireData {
-    private int Helpful;
-    private boolean NoAns_Helpful;
-    private int Quality;
-    private boolean NoAns_Quality;
-    private int OverallMood;
-    private boolean NoAns_Mood;
-    private int Recommend;
-    private boolean NoAns_Recommend;
-    private int Easy;
-    private boolean NoAns_Easy;
-    private int Interest;
-    private boolean NoAns_Interest;
-    private int LikeGral;
-    private boolean NoAns_Like;
-    private int likedLooks;
-    private boolean NoAns_Looks;
-    private int Privacy;
-    private boolean NoAns_Privacy;
-    private int UnderstandAssessment;
-    private boolean NoAns_UnderstandAssessment;
-    private int UnderstandTraining;
-    private boolean NoAns_UnderstandTraining;
-    private int TrustInfo;
-    private boolean NoAns_TrustInfo;
-    private int Problems;
-    private boolean NoAns_Problems;
-    private String IdealSessions;
-    private String WhyIdeal;
-    private int Tiring;
-    private boolean NoAns_Tiring;
-    private int Distracted;
-    private boolean noAns_Distracted;
-    private int Similar;
-    private boolean NoAns_Similar;
-    private int OtherTreatment;
-    private boolean NoAns_OtherTreatment;
-    private boolean Home;
-    private boolean Work;
-    private boolean Public;
-    private boolean Commute;
-    private boolean Vacation;
-    private boolean OtherComplete;
-    private String Other_Place;
-    private int NoAns_where;
+    @NotNull
+    private Integer helpful;
+    @NotNull
+    private Integer quality;
+    @NotNull
+    private Integer overallMood;
+    @NotNull
+    private Integer recommend;
+    @NotNull
+    private Integer easy;
+    @NotNull
+    private Integer interest;
+    @NotNull
+    private Integer likeGral;
+    @NotNull
+    private Integer likedLooks;
+    @NotNull
+    private Integer privacy;
+    @NotNull
+    private Integer understandAssessment;
+    @NotNull
+    private Integer understandTraining;
+    @NotNull
+    private Integer trustInfo;
+    @NotNull
+    private Integer problems;
+    @NotNull
+    private String idealSessions;
+    @NotNull
+    private String whyIdeal;
+    @NotNull
+    private Integer tiring;
+    @NotNull
+    private Integer distracted;
+    @NotNull
+    private Integer similarProgram;
+    private Integer otherTreatment;
+
+    private Boolean home;
+    private Boolean work;
+    private Boolean publicPlace;
+    private Boolean commute;
+    private Boolean vacation;
+    private Boolean otherComplete;
+    private String otherPlace;
+    private Integer NoAnsWhere;
     @Column(name="evalCondition") // 'Condition' is a reserved word in some databases.
-    private int Condition;
+    @NotNull
+    private Integer condition;
 
 
 }
