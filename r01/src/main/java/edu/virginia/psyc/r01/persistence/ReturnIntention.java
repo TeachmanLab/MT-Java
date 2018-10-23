5104e5b652e61fd51894a266cff864fae20faca9
Noah French <noahjfrench@gmail.com>
2018-10-23 14:36:48 -0400

    Add @NotNull validation to the Return Intention Java file. Keep all the form hydration logic in the HTML, because that form depends on Javascript to conditionally display a text box.

--- r01/src/main/java/edu/virginia/psyc/r01/persistence/ReturnIntention.java
+++ r01/src/main/java/edu/virginia/psyc/r01/persistence/ReturnIntention.java
@@ -7,6 +7,7 @@ import org.mindtrails.domain.questionnaire.LinkedQuestionnaireData;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Diheng on 7/27/17.
@@ -17,7 +18,9 @@ import javax.persistence.Table;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ReturnIntention extends LinkedQuestionnaireData {
-    private int returnIntention;
+
+    @NotNull
+    private Integer returnIntention;
 
     @Lob
     private String notReturnReason;
