43b299f5eca4bfb3e8606514529e5a32b014ecee
Noah French <noahjfrench@gmail.com>
2018-10-30 02:09:56 -0400

    Rework the 'All things considered, how satisifed are you with your life as a whole?' question to be a dropdown menu. Because there are so many (12) different options, this allows the question to be easily doable on both desktop and mobile.

--- r01/src/main/java/edu/virginia/psyc/r01/persistence/Wellness.java
+++ r01/src/main/java/edu/virginia/psyc/r01/persistence/Wellness.java
@@ -21,62 +21,46 @@ public class Wellness extends LinkedQuestionnaireData {
 
     // Life Satisfaction
     @NotNull
-    @MeasureField(order=1, desc="", group="satisfaction")
     Integer satisfaction;
 
     // NGSES
     @NotNull
-    @MeasureField(order=2, desc="When facing difficult tasks, I am certain that I will accomplish them.", group="ngses")
+    @MeasureField(order=1, desc="When facing difficult tasks, I am certain that I will accomplish them.", group="ngses")
     Integer accomplishTasks;
     @NotNull
-    @MeasureField(order=3, desc="I am confident that I can perform well on many different tasks.", group="ngses")
+    @MeasureField(order=2, desc="I am confident that I can perform well on many different tasks.", group="ngses")
     Integer performMultitask;
     @NotNull
-    @MeasureField(order=4, desc="Compared to other people, I can do most tasks very well.", group="ngses")
+    @MeasureField(order=3, desc="Compared to other people, I can do most tasks very well.", group="ngses")
     Integer doMost;
 
     // Growth Mindset
     @NotNull
-    @MeasureField(order=5, desc="You can learn new things, but you can't really change how you think.", group="growth")
+    @MeasureField(order=4, desc="You can learn new things, but you can't really change how you think.", group="growth")
     Integer learn;
     @NotNull
-    @MeasureField(order=6, desc="No matter how much you have been thinking a particular way, you can always change it quite a bit.", group="growth")
+    @MeasureField(order=5, desc="No matter how much you have been thinking a particular way, you can always change it quite a bit.", group="growth")
     Integer particularThinking;
     @NotNull
-    @MeasureField(order=7, desc="You can always substantially change how you think.", group="growth")
+    @MeasureField(order=6, desc="You can always substantially change how you think.", group="growth")
     Integer alwaysChangeThinking;
 
     // LOT-R
     @NotNull
-    @MeasureField(order=8, desc="If something can go wrong with me, it will.", group="lot-r")
+    @MeasureField(order=7, desc="If something can go wrong with me, it will.", group="lot-r")
     Integer wrongWill;
     @NotNull
-    @MeasureField(order=9, desc="I hardly ever expect things to go my way.", group="lot-r")
+    @MeasureField(order=8, desc="I hardly ever expect things to go my way.", group="lot-r")
     Integer hardlyEver;
-
-    @Override
-    public Map<String, String> getGroupDescriptions() {
-        Map<String, String> desc = new TreeMap<>();
-        desc.put("satisfaction", "All things considered, how satisfied are you with your life as a whole?");
-        return Collections.unmodifiableMap(desc);
-    }
 
     @Override
     public Map<Integer, String> getScale(String group) {
         Map<Integer, String> tmpScale = new TreeMap<>();
-        if (group.equals("satisfaction")) {
-            tmpScale.put(0, "Completely dissatisfied");
-            for (int i = 1; i < 10; i++) {
-                tmpScale.put(i, "");
-            }
-            tmpScale.put(10, "Completely satisfied");
-        } else {
             tmpScale.put(0, "Strongly disagree");
             tmpScale.put(1, "Disagree");
             tmpScale.put(2, "Neutral");
             tmpScale.put(3, "Agree");
             tmpScale.put(4, "Strongly agree");
-        }
         return Collections.unmodifiableMap(tmpScale);
     }
 }
