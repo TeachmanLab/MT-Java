8f639f089a7e2ba52115d3c80d1b6c75a9d7c212
Noah French <noahjfrench@gmail.com>
2019-01-09 22:04:24 -0500

    Finish updating the mental health history page.

--- r01/src/main/java/edu/virginia/psyc/r01/persistence/MentalHealthHistory.java
+++ r01/src/main/java/edu/virginia/psyc/r01/persistence/MentalHealthHistory.java
@@ -15,41 +15,11 @@ import javax.validation.constraints.NotNull;
 @Data
 public class MentalHealthHistory extends LinkedQuestionnaireData {
 
-    /*
-    Drop the following tables:
-    1.mental_health_disorders;
-    2.mental_health_disorders_past;
-    3.no_help_reason;
-    4.helps_past;
-    5.help;
-    6.help_seeking;
-    7.health_change;
-    8.help_change;
-    9.mental_health_history;
-     */
-
-
     @ElementCollection
     @CollectionTable(name = "mental_health_disorders", joinColumns = @JoinColumn(name = "id"))
-    @Column(name = "disorder")
+    @Column(name = "disorders")
     private List<String> disorders;
-
-    private int gad;
-    private int ocd;
-    private int panic_disorder;
-    private int agoraphobia;
-    private int ptsd;
-    private int social_anxiety;
-    private int phobias;
-    private int dementia;
-    private int substance_use_disorder;
-    private int schizophrenia;
-    private int depression;
-    private int bipolar;
-    private int eating;
-    private int personality;
-    private int disorders_other;
-    private String disorders_other_text;
+    private String otherDisorder;
 
     @ElementCollection
     @CollectionTable(name = "mental_health_help", joinColumns = @JoinColumn(name = "id"))
