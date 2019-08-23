6d5172743df4330407008d65315ed791602b5438
Dan Funk <daniel.h.funk@gmail.com>
2019-08-23 09:42:47 -0400

    Fixing the login prompts during the reset password process so people know what the heck is going on.

--- core/src/main/java/org/mindtrails/controller/LoginController.java
+++ core/src/main/java/org/mindtrails/controller/LoginController.java
@@ -125,7 +125,8 @@ public class LoginController {
         participantService.save(p);
 
         emailService.sendPasswordReset(p);
-        return("redirect:login");
+        model.addAttribute("resetSent", true);
+        return("login");
     }
 
     @RequestMapping(value="/resetPassStep2/{token}", method = RequestMethod.GET)
@@ -188,7 +189,8 @@ public class LoginController {
         Authentication auth = new UsernamePasswordAuthenticationToken( participant.getEmail(), participant.getPassword());
         SecurityContextHolder.getContext().setAuthentication(auth);
         LOG.info("Participant authenticated.");
-        return "redirect:/session";
+        model.addAttribute("reset", true);
+        return "login";
     }
 
     /**
