9f3e97e9caaf164ab9c968adf653ff5ee9c72e61
Dan Funk <daniel.h.funk@gmail.com>
2020-04-01 11:51:21 -0400

    timeonpage was getting added twice (once when using the radio options fragment) stetched out the entry for questions.

--- r01/src/main/java/edu/virginia/psyc/r01/domain/R01Study.java
+++ r01/src/main/java/edu/virginia/psyc/r01/domain/R01Study.java
@@ -143,6 +143,7 @@ public class R01Study extends BaseStudy {
         pretest.addTask(new Task("Comorbid","Mood and Drinking Patterns", Task.TYPE.questions, 0 ));
         pretest.addTask(new Task("Wellness","What I Believe", Task.TYPE.questions, 0 ));
         pretest.addTask(new Task("Mechanisms","How I Respond", Task.TYPE.questions, 0 ));
+        pretest.addTask(new Task("Covid19","COVID-19", Task.TYPE.questions, 0 ));
         pretest.addTask(new Task("TechnologyUse", "Technology Use", Task.TYPE.questions, 0));
         sessions.add(pretest);
 
@@ -191,6 +192,7 @@ public class R01Study extends BaseStudy {
         session3.addTask(new Task("Comorbid","Mood and Drinking Patterns", Task.TYPE.questions, 0 ));
         session3.addTask(new Task("Wellness","What I Believe", Task.TYPE.questions, 0 ));
         session3.addTask(new Task("Mechanisms","How I Respond", Task.TYPE.questions, 0 ));
+        session3.addTask(new Task("Covid19","COVID-19", Task.TYPE.questions, 0 ));
         session3.addTask(new Task("ReturnIntention","Returning to the Program", Task.TYPE.questions, 0));
 
         sessions.add(session3);
@@ -225,6 +227,7 @@ public class R01Study extends BaseStudy {
         session5.addTask(new Task("Comorbid","Mood and Drinking Patterns", Task.TYPE.questions, 0 ));
         session5.addTask(new Task("Wellness","What I Believe", Task.TYPE.questions, 0 ));
         session5.addTask(new Task("Mechanisms","How I Respond", Task.TYPE.questions, 0 ));
+        session5.addTask(new Task("Covid19","COVID-19", Task.TYPE.questions, 0 ));
         session5.addTask(new Task("HelpSeeking","Change in Help-Seeking Behavior", Task.TYPE.questions, 1));
         session5.addTask(new Task("Evaluation","Evaluating the Program", Task.TYPE.questions, 2));
         session5.addTask(new Task("AssessingProgram","Assessing the Program", Task.TYPE.questions, 2));
@@ -243,6 +246,7 @@ public class R01Study extends BaseStudy {
         post.addTask(new Task("Comorbid","Mood and Drinking Patterns", Task.TYPE.questions, 0 ));
         post.addTask(new Task("Wellness","What I Believe", Task.TYPE.questions, 0 ));
         post.addTask(new Task("Mechanisms","How I Respond", Task.TYPE.questions, 0 ));
+        post.addTask(new Task("Covid19","COVID-19", Task.TYPE.questions, 0 ));
         post.addTask(new Task("HelpSeeking","Change in Help-Seeking Behavior", Task.TYPE.questions, 1));
         sessions.add(post);
 
