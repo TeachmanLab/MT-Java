e26c2e21b571208f542995e75abac4f6097b5cc7
Henry <hb7zz@virginia.edu>
2021-03-25 17:35:55 -0400

    2021-03-25: Pushing up latest changes for Spanish site, mainly updates to menu, progress, and account pages; switched to shorter version of Acculturation measure

--- spanish/src/main/java/edu/virginia/psyc/spanish/persistence/Acculturation.java
+++ spanish/src/main/java/edu/virginia/psyc/spanish/persistence/Acculturation.java
@@ -22,38 +22,14 @@ public class Acculturation extends LinkedQuestionnaireData {
     @MeasureField(order = 1, desc = "In general, what languages(s) do you read and speak?", group = "languages1")
     private Integer general;
     @NotNull
-    @MeasureField(order = 2, desc = "What was the language(s) you used as a child?", group = "languages2")
-    private Integer child;
-    @NotNull
-    @MeasureField(order = 3, desc = "What language(s) do you usually speak at home?", group = "languages2")
+    @MeasureField(order = 2, desc = "What language(s) do you usually speak at home?", group = "languages2")
     private Integer home;
     @NotNull
-    @MeasureField(order = 4, desc = "In which language(s) do you usually think?", group = "languages2")
+    @MeasureField(order = 3, desc = "In which language(s) do you usually think?", group = "languages2")
     private Integer think;
     @NotNull
-    @MeasureField(order = 5, desc = "What language(s) do you usually speak with your friends?", group = "languages2")
+    @MeasureField(order = 4, desc = "What language(s) do you usually speak with your friends?", group = "languages2")
     private Integer speak;
-    @NotNull
-    @MeasureField(order = 6, desc = "In what language(s) are the TV programs you usually watch?", group = "languages2")
-    private Integer tv;
-    @NotNull
-    @MeasureField(order = 7, desc = "In what language(s) are the radio programs you usually listen to?", group = "languages2")
-    private Integer radio;
-    @NotNull
-    @MeasureField(order = 8, desc = "In general, in what language(s) are the movies, TV and radio programs you prefer to watch and listen to?", group = "languages2")
-    private Integer prefer;
-    @NotNull
-    @MeasureField(order = 9, desc = "Your close friends are:", group = "friends")
-    private Integer friends;
-    @NotNull
-    @MeasureField(order = 10, desc = "You prefer going to social gatherings/parties at which the people are:", group = "friends")
-    private Integer parties;
-    @NotNull
-    @MeasureField(order = 11, desc = "The persons you visit or who visit you are:", group = "friends")
-    private Integer visit;
-    @NotNull
-    @MeasureField(order = 12, desc = "If you could choose your children’s friends, you would want them to be:", group = "friends")
-    private Integer choose;
 
     @Override
     public Map<Integer, String> getScale(String group) {
@@ -72,13 +48,6 @@ public class Acculturation extends LinkedQuestionnaireData {
                 tmpScale.put(3, "acculturation.both_equally");
                 tmpScale.put(4, "acculturation.more_english_than_spanish");
                 tmpScale.put(5, "acculturation.only_english");
-                break;
-            case ("friends"):
-                tmpScale.put(1, "acculturation.all_latinos_hispanics");
-                tmpScale.put(2, "acculturation.more_latinos_than_hispanics");
-                tmpScale.put(3, "acculturation.about_half_and_half");
-                tmpScale.put(4, "acculturation.more_americans_than_hispanics");
-                tmpScale.put(5, "acculturation.all_americans");
                 break;
         }
         return Collections.unmodifiableMap(tmpScale);
